752d835f993a72ef081fee99a3f086d5e670121a
duchuyvo <voduchuy016688@gmail.com>
2025-04-22 11:20:57 +0700

    add domain

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BASE_URL: 'https://54.255.249.94/api/v1',
+    BASE_URL: 'https://voduchuytravelbooking.publicvm.com/api/v1',
     TIMEOUT: 10000,
     HEADERS: {
         'Content-Type': 'application/json',

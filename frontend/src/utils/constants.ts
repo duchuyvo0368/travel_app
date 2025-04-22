b3080a5ef3cef20b4a6635f4769a23f001040a34
duchuyvo <voduchuy016688@gmail.com>
2025-04-22 11:05:12 +0700

    add ssl

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BASE_URL: 'http://localhost:4001/api/v1',
+    BASE_URL: 'https://54.255.249.94/api/v1',
     TIMEOUT: 10000,
     HEADERS: {
         'Content-Type': 'application/json',
